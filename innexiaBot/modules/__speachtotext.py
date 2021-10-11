365ad305bb469cce89d01e50744c4b6d26b12700
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2021-10-11 18:37:59 +0530

    Update __speachtotext.py

--- innexiaBot/modules/__speachtotext.py
+++ innexiaBot/modules/__speachtotext.py
@@ -33,7 +33,7 @@ async def _(event):
         return
     if event.is_group:
      if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-       await event.reply("🚨 Need Admin Pewer.. You can't use this command.. But you can use in my pm")
+       await event.reply("🚨 Need Admin Power.. You can't use this command.. But you can use in my pm")
        return
 
     start = datetime.now()
