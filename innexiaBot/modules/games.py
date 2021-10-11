bd07851bc257661c5ab7863e113fecb4514c657b
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2021-10-11 19:26:23 +0530

    Update games.py

--- innexiaBot/modules/games.py
+++ innexiaBot/modules/games.py
@@ -11,7 +11,7 @@ async def _(event):
     r = await event.reply(file=InputMediaDice(""))
     input_int = int(input_str)
     if input_int > 6:
-        await event.reply("hey nigga use number 1 to 6 only")
+        await event.reply("hey bruh use number 1 to 6 only")
     
     else:
         try:
@@ -31,7 +31,7 @@ async def _(event):
     r = await event.reply(file=InputMediaDice("🎯"))
     input_int = int(input_str)
     if input_int > 6:
-        await event.reply("hey nigga use number 1 to 6 only")
+        await event.reply("hey bruh use number 1 to 6 only")
     
     else:
         try:
@@ -51,7 +51,7 @@ async def _(event):
     r = await event.reply(file=InputMediaDice("🏀"))
     input_int = int(input_str)
     if input_int > 5:
-        await event.reply("hey nigga use number 1 to 6 only")
+        await event.reply("hey bruh use number 1 to 6 only")
     
     else:
         try:
